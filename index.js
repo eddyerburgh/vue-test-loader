5d5ad0dc229b6aa5fb8d1ae789389e5c3c936df6
eddyerburgh <edward.yerburgh@gmail.com>
2017-11-09 18:14:38 +0000

    feat(extension): add extension option

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const fs = require('fs')
 const babel = require('babel-core')
 const babelPluginTransformRelativePaths = require('babel-plugin-transform-relative-paths')
+const loaderUtils = require('loader-utils')
 
 module.exports = function(source) {
+  const options = loaderUtils.getOptions(this);
+
   const filename = this.resourcePath.substr(this.resourcePath.lastIndexOf('/') + 1).split('.')[0]
 
-  const extension = '.spec.js'
-  const dir = `${this.context}/${this.options.extension || '__tests__'}`
+  const extension = options.extension || '.spec.js'
+  const dir = `${this.context}/__tests__`
 
   if (!fs.existsSync(dir)){
       fs.mkdirSync(dir);
